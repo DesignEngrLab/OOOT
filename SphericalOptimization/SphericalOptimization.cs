bd9e9c66fb445e452ff3586a2a3499947620f60e
Matt Campbell <micampbell@users.noreply.github.com>
2021-10-07 20:59:29 -0700

    old commit. not really sure what's changed

--- SphericalOptimization/SphericalOptimization.cs
+++ SphericalOptimization/SphericalOptimization.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -36,11 +37,9 @@ namespace SphericalOptimization
         /// </summary>
         private readonly double sigma = 0.5;
 
-        /// <summary>
-        /// The vertices
-        /// </summary>
-        public readonly SortedList<double, double[]> vertices =
-            new SortedList<double, double[]>(new optimizeSort(optimize.maximize));
+
+        public SortedList<double, double[]> sortedBest { get; private set; }
+
 
         #endregion
 
@@ -51,6 +50,7 @@ namespace SphericalOptimization
         /// </summary>
         public SphericalOptimization()
         {
+            x = new double[2];
             RequiresObjectiveFunction = true;
             ConstraintsSolvedWithPenalties = true;
             InequalitiesConvertedToEqualities = false;
@@ -94,15 +94,10 @@ namespace SphericalOptimization
         protected override double run(out double[] xStar)
         {
             var icosaEdgeScores = new double[30];
+            // first evaluate all the 30 faces of a triacontahedron
             for (int i = 0; i < PlatonicConstants.TriacontahedronDirections.Length; i++)
-            {
-                x = PlatonicConstants.TriacontahedronDirections[i];
-                var fLocal = calc_f(x);
-                vertices.Add(fLocal, x);
-                icosaEdgeScores[i] = fLocal;
-            }
-            //xStar = null;
-            //return 0;
+                icosaEdgeScores[i] = calc_f(PlatonicConstants.TriacontahedronDirections[i]);
+            // second, find icosahedron faces that are not worth pursuing
             var icosaFacesToIgnore = new HashSet<int>();
             for (int i = 0; i < PlatonicConstants.IcosaEdgeToFaces.Length; i++)
             {
@@ -115,20 +110,37 @@ namespace SphericalOptimization
                 if (otherRightTriaContaIndices.Item1 >= otherLeftTriaContaIndices.Item2)
                     icosaFacesToIgnore.Add(rightFaceIndex);
             }
+            //third, complete barycentric nelder mead for icosahedron faces that are not in the ignore list
+            var bestResults = new ConcurrentBag<(double, double[], int)>();
             for (int i = 0; i < 20; i++)
             {
                 if (icosaFacesToIgnore.Contains(i)) continue;
                 var vertexNormals = PlatonicConstants.IcosaTriangleVertices[i]
                     .Select(j => PlatonicConstants.DodechedronDirections[j]).ToArray();
-                var oneFStar = BarycentricSphericalNelderMead(vertexNormals, PlatonicConstants.IcosaTriangleEdges[i]
+                var thisFStar = BarycentricSphericalNelderMead(vertexNormals, PlatonicConstants.IcosaTriangleEdges[i]
                     .Select(v => icosaEdgeScores[v]).ToList(), out double[] thisXStar);
+                bestResults.Add((thisFStar, thisXStar, i));
             }
-            xStar = vertices.Values[0];
-            return vertices.Keys[0];
+            sortedBest = new SortedList<double, double[]>(new optimizeSort(optimize.minimize));
+            foreach (var result in bestResults)
+                sortedBest.Add(result.Item1, ConvertTo3DUnitVector(result.Item2,
+                    PlatonicConstants.IcosaTriangleVertices[result.Item3]
+                    .Select(j => PlatonicConstants.DodechedronDirections[j]).ToArray()));
+            for (int i = sortedBest.Count - 2; i >= 0; i--)
+            {
+                if (sortedBest.Keys[i].IsPracticallySame(sortedBest.Keys[i + 1], 1e-10) && 
+                    sortedBest.Values[i].dotProduct(sortedBest.Values[i+1]).IsPracticallySame(1.0, 1e-3))
+                    sortedBest.RemoveAt(i + 1);
+            }
+            xStar = sortedBest.Values[0];
+            return sortedBest.Keys[0];
         }
-        private double BarycentricSphericalNelderMead(double[][] vertexNormals, List<double> list, out double[] thisXStar)
+        private double BarycentricSphericalNelderMead(double[][] vertexNormals, List<double> initialFValues, out double[] thisXStar)
         {
-
+            var vertices = new SortedList<double, double[]>(new optimizeSort(optimize.minimize));
+            vertices.Add(initialFValues[0], new[] { 0.5, 0.5 });
+            vertices.Add(initialFValues[1], new[] { 0.0, 0.5 });
+            vertices.Add(initialFValues[2], new[] { 0.5, 0.0 });
             while (notConverged(k, numEvals, vertices.Keys[0], vertices.Values[0], vertices.Values))
             {
                 #region Compute the REFLECTION POINT
@@ -146,7 +158,8 @@ namespace SphericalOptimization
                 var Xr = CloneVertex(vertices.Values[n]);
                 for (var i = 0; i < n; i++)
                     Xr[i] = (1 + rho) * Xm[i] - rho * Xr[i];
-                var fXr = calc_f(Xr);
+                var fXr = OutsideOfTriangle(Xr) ? double.PositiveInfinity :
+                    calc_f(ConvertTo3DUnitVector(Xr, vertexNormals));
                 SearchIO.output("x_r = " + MakePrintString(Xr), 4);
                 #endregion
 
@@ -158,7 +171,8 @@ namespace SphericalOptimization
                     var Xe = CloneVertex(vertices.Values[n]);
                     for (var i = 0; i < n; i++)
                         Xe[i] = (1 + rho * chi) * Xm[i] - rho * chi * Xe[i];
-                    var fXe = calc_f(Xe);
+                    var fXe = OutsideOfTriangle(Xe) ? double.PositiveInfinity :
+                    calc_f(ConvertTo3DUnitVector(Xe, vertexNormals));
                     #endregion
 
                     vertices.RemoveAt(n);  // remove the worst
@@ -178,7 +192,7 @@ namespace SphericalOptimization
 
                 else
                 {
-                    #region but it's better than second worst, still do reflect
+                    #region but if it's better than second worst, still do reflect
 
                     if (fXr < vertices.Keys[n - 1])
                     {
@@ -196,7 +210,8 @@ namespace SphericalOptimization
                             var Xc = CloneVertex(vertices.Values[n]);
                             for (var i = 0; i < n; i++)
                                 Xc[i] = (1 + rho * psi) * Xm[i] - rho * psi * Xc[i];
-                            var fXc = calc_f(Xc);
+                            var fXc = OutsideOfTriangle(Xc) ? double.PositiveInfinity :
+                                calc_f(ConvertTo3DUnitVector(Xc, vertexNormals));
 
                             if (fXc <= fXr)
                             {
@@ -219,7 +234,11 @@ namespace SphericalOptimization
                                     vertices.RemoveAt(j);
                                 }
                                 for (int j = 0; j < n; j++)
-                                    vertices.Add(calc_f(newXs[j]), newXs[j]);
+                                {
+                                    var Xs = newXs[j];
+                                    //if (!OutsideOfTriangle(Xs))
+                                    vertices.Add(calc_f(ConvertTo3DUnitVector(Xs, vertexNormals)), Xs);
+                                }
                                 SearchIO.output("shrink towards best", 4);
                             }
 
@@ -232,7 +251,8 @@ namespace SphericalOptimization
                             var Xcc = CloneVertex(vertices.Values[n]);
                             for (var i = 0; i < n; i++)
                                 Xcc[i] = (1 - psi) * Xm[i] + psi * Xcc[i];
-                            var fXcc = calc_f(Xcc);
+                            var fXcc = OutsideOfTriangle(Xcc) ? double.PositiveInfinity :
+                                calc_f(ConvertTo3DUnitVector(Xcc, vertexNormals));
 
                             if (fXcc < vertices.Keys[n])
                             {
@@ -251,12 +271,16 @@ namespace SphericalOptimization
                                     var Xs = CloneVertex(vertices.Values[j]);
                                     for (var i = 0; i < n; i++)
                                         Xs[i] = vertices.Values[0][i]
-                                                    - sigma * (Xs[i] - vertices.Values[0][i]);
+                                                    + sigma * (Xs[i] - vertices.Values[0][i]);
                                     newXs.Add(Xs);
                                     vertices.RemoveAt(j);
                                 }
                                 for (int j = 0; j < n; j++)
-                                    vertices.Add(calc_f(newXs[j]), newXs[j]);
+                                {
+                                    var Xs = newXs[j];
+                                    //if (!OutsideOfTriangle(Xs))
+                                    vertices.Add(calc_f(ConvertTo3DUnitVector(Xs, vertexNormals)), Xs);
+                                }
                                 SearchIO.output("shrink towards best and flip", 4);
                             }
 
@@ -268,15 +292,35 @@ namespace SphericalOptimization
                 #endregion
 
                 k++;
-                SearchIO.output("iter. = " + k, 2);
-                SearchIO.output("Fitness = " + vertices.Keys[0], 2);
+                SearchIO.output("iter. = " + k, 1);
+                SearchIO.output("Fitness = " + vertices.Keys[0], 1);
             } // END While Loop
             thisXStar = vertices.Values[0];
-           var thisfStar = vertices.Keys[0];
+            var thisfStar = vertices.Keys[0];
             vertices.Clear();
             return thisfStar;
         }
 
+        private bool OutsideOfTriangle(double[] barycentricCoords)
+        {
+            if (barycentricCoords[0] < 0) return true;
+            if (barycentricCoords[1] < 0) return true;
+            return 1 - barycentricCoords[0] - barycentricCoords[1] < 0;
+        }
+
+        private double[] ConvertTo3DUnitVector(double[] coords, double[][] vertexNormals)
+        {
+            var a = coords[0];
+            var b = coords[1];
+            var c = 1 - a - b;
+            var dir = new[]
+            {
+                a * vertexNormals[0][0] + b * vertexNormals[1][0] + c * vertexNormals[2][0],
+                a * vertexNormals[0][1] + b * vertexNormals[1][1] + c * vertexNormals[2][1],
+                a * vertexNormals[0][2] + b * vertexNormals[1][2] + c * vertexNormals[2][2]
+            };
+            return dir.normalize();
+        }
 
         private (double, double) FindTriacontahedronValuesExceptCommon(int faceIndex, int edgeIndexToIgnore, double[] icosaEdgeScores)
         {
--- SphericalOptimizationTest/Program.cs
+++ SphericalOptimizationTest/Program.cs
@@ -5,6 +5,7 @@ using TVGL;
 using OptimizationToolbox;
 using System.Diagnostics;
 using System.Collections.Generic;
+using System.IO;
 
 namespace SphericalOptimization
 {
@@ -16,33 +17,39 @@ namespace SphericalOptimization
             Parameters.Verbosity = OptimizationToolbox.VerbosityLevels.Everything;
             // this next line is to set the Debug statements from OOOT to the Console.
             Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
-            //var optMethod = new NelderMead();
-            var optMethod = new SphericalOptimization();
+            var random = new Random();
+            foreach (var filename in Directory.GetFiles(@"C:\Users\campmatt\source\repos\medemaMVP\TestFiles", "*.stl", SearchOption.AllDirectories)
+                .OrderBy(x=>random.NextDouble()))
+            {
+                TVGL.IOFunctions.IO.Open(filename, out TessellatedSolid ts);
+                Console.WriteLine("optimizing...");
 
-            TVGL.IOFunctions.IO.Open(@"C:\Users\campmatt\source\repos\medemaMVP\TestFiles\TopOp\Part2.stl", out TessellatedSolid ts);
-            //ts.Transform(TVGL.Numerics.Matrix4x4.CreateRotationY(1.5));
-            //ts.Transform(TVGL.Numerics.Matrix4x4.CreateRotationZ(1.25));
-            optMethod.Add(new FloorAndWallFaceScore(ts));
-            optMethod.Add(new DeltaXConvergence(1e-6));
-            /* Let us start the search from a specific point. */
-            double[] xInit = new[] { 0.0, 0.0 };//,100};
-            var fStar = optMethod.Run(out var xStar, xInit);
-            PlotDirections(optMethod.vertices.Keys, optMethod.vertices.Values, ts);
-            /* output is provided from the optimization. Since the optMethod is an object
-             * we can probe it following the run to get at important data like how the 
-             * process converged. */
-            Console.WriteLine("Convergence Declared by " + optMethod.ConvergenceDeclaredByTypeString);
-            Console.WriteLine("X* = " + xStar.MakePrintString());
-            Console.WriteLine("F* = " + fStar, 1);
-            Console.WriteLine("NumEvals = " + optMethod.numEvals);
-            /* Since there is no randomness in the process, you should get the following
-             * response:
-             * No inequalities specified.
-             * Convergence Declared by ToKnownBestFConvergence
-             * X* = {   1.079   ,  1.159    }
-             * F* = 0.00772036716239199
-             * NumEvals = 245
-             */
+                var optMethod = new SphericalOptimization();
+
+                //ts.Transform(TVGL.Numerics.Matrix4x4.CreateRotationY(1.5));
+                //ts.Transform(TVGL.Numerics.Matrix4x4.CreateRotationZ(1.25));
+                optMethod.Add(new FloorAndWallFaceScore(ts));
+                optMethod.Add(new MaxSpanInPopulationConvergence(1e-3));
+                /* Let us start the search from a specific point. */
+                double[] xInit = new[] { 0.0, 0.0 };//,100};
+                var fStar = optMethod.Run(out var xStar, xInit);
+                PlotDirections(optMethod.sortedBest.Keys, optMethod.sortedBest.Values, ts);
+                /* output is provided from the optimization. Since the optMethod is an object
+                 * we can probe it following the run to get at important data like how the 
+                 * process converged. */
+                Console.WriteLine("Convergence Declared by " + optMethod.ConvergenceDeclaredByTypeString);
+                Console.WriteLine("X* = " + xStar.MakePrintString());
+                Console.WriteLine("F* = " + fStar, 1);
+                Console.WriteLine("NumEvals = " + optMethod.numEvals);
+                /* Since there is no randomness in the process, you should get the following
+                 * response:
+                 * No inequalities specified.
+                 * Convergence Declared by ToKnownBestFConvergence
+                 * X* = {   1.079   ,  1.159    }
+                 * F* = 0.00772036716239199
+                 * NumEvals = 245
+                 */
+            }
             Console.ReadKey();
         }
         static void PlotDirections(IList<double> keys, IList<double[]> values, TessellatedSolid ts)
@@ -56,9 +63,9 @@ namespace SphericalOptimization
             var colors = new List<TVGL.Color>();
             for (int i = 0; i < numToShow; i++)
             {
-                plotVertices.Add(new Vertex(0.03*(19+(keys[i]/max)) * (new TVGL.Numerics.Vector3(values[i]))));
-                colors.Add(TVGL.Color.HSVtoRGB((float)rankFraction[i], 1, 1));
-                //colors.Add(TVGL.Color.HSVtoRGB((float)(keys[i]/max), 1, 1));
+                plotVertices.Add(new Vertex(0.03 * (19 + (keys[i] / max)) * (new TVGL.Numerics.Vector3(values[i]))));
+                //colors.Add(TVGL.Color.HSVtoRGB((float)rankFraction[i], 1, 1));
+                colors.Add(TVGL.Color.HSVtoRGB((float)(keys[i]/max), 1, 1));
                 Console.WriteLine(keys[i]);
             }
             Presenter.ShowGaussSphereWithIntensity(plotVertices, colors, ts);
